bcedf46feb4557061258450a2612d5db13b57c1f
EDA <eda@Mac-Mini-4.local>
2017-05-19 12:22:17 +1200

    added addPost

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import {HashRouter as Router,Route,Link} from 'react-router-dom'
+import List from './List'
 
-const App = () => {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      
+      posts:[
+        {title: ''},
+        {message: ''},
+        {comments:['']}
+      ],
+
+    }
+  }
+
+addPost(post) {
+  const newPosts = [].concat(this.state.posts)
+  newPosts.push(post)
+  this.setState({
+    posts:newPosts
+    })
+  }
+
+render() {
   return (
-    <h1>React development has begun!</h1>
-  )
+<Router>
+  <div>
+    <AddPost content = {this.addPost} />
+    <List />
+  </div>
+   </Router>
+    )
+  }
 }
-
 export default App
 
